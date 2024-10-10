538708d244194ea7eee796b5da065b57c3563282
fatimanavroski2008 <133028934+fatimanavroski2008@users.noreply.github.com>
2024-10-10 11:36:15 -0300

    Add files via upload

---
+++ fatima.4/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Quando a Nova Zelândia foi descoberta?',
+    'A Nova Zelândia foi descoberta no ano de 0000, '
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o tipo de politica adotada na Nova Zelândia?',
+    'A politíca adotada na Nova Zelândia é o Parlamentarismo'
+)
+
+criaCartao(
+    'Cultura',
+    'Qual a lingua oficial da Nova Zelândia?',
+    'O idioma oficial da Nova Zelândia é o Inglês, porém possui muita influencia na lingua Maori, por ter sido colonizada por esse povo'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
