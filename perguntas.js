e6da87f5b8bf463633849bbde787aab0aa8ec6b3
fatimanavroski2008 <133028934+fatimanavroski2008@users.noreply.github.com>
2024-10-17 11:19:49 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Quando a Nova Zelândia foi descoberta?',
+    'A Nova Zelândia foi descoberta em 1769 quando o navegador Britânico Capitão James Cook anunciou o começo da colonização Européia na Nova Zelândia. '
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o tipo de politica adotada na Nova Zelândia?',
+    'A politíca adotada na Nova Zelândia é o Parlamentarismo'
+)
+
+criaCartao(
+    'Cultura',
+    'Quais são as linguas oficiais da Nova Zelândia?',
+    'Na Nova Zelândia existem dois idiomas oficiais (além da Língua de Sinais): o inglês e o maori — o idioma do povo nativo de lá, que também é ensinado nas escolas de educação básica.'
+)
+
+criaCartao(
+    'Curiosidade',
+    'Mais ovelhas do que gente?  ',
+    'A Nova Zelândia é um país pouco populoso, tem por volta de 5 milhões de habitantes (um número bem menor que o da cidade de São Paulo, por exemplo). Portanto, é bastante comum que o número de animais criados pela pecuária, como as ovelhas, sejam maior do que o de pessoas. A pecuária é uma atividade muito forte e chega a movimentar 40 milhões de ovelhas e 10 milhões de cabeças de gado.'
+)
